0011c1f2fb3d27b318628ac781f2de7c31be05c3
tunahari <Tunahari86@gmail.com>
2024-10-04 23:02:10 +0700

    OrderServive
    Đang làm sevice. được 1 ít rồi

---
+++ OrderAPI/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderServices.Implementation;
+using OrderServices.Interface;
+
+namespace OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            this.orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var response = await orderService.GetAll();
+            return Ok(response);
+        }
+    }
+   
+}
