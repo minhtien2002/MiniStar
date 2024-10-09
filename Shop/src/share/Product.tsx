2cfa95aba18b49d26f783157acb2135d8e392c07
minhtien <124766467+minhtien2002@users.noreply.github.com>
2024-10-09 17:30:20 +0700

    Update Product.tsx

--- Shop/src/share/Product.tsx
+++ Shop/src/share/Product.tsx
@@ -9,9 +9,9 @@ interface ProductProps {
 
 const Product: React.FC<ProductProps> = ({ ProductId, ProductName, Price, ProductImage }) => {
   return (
-    <div className={"flex flex-col justify-center items-center border border-white my-8 p-6 rounded-md shadow-lg hover:border-solid hover:border hover:border-[#34A853]"}>
+    <div className={"flex flex-col justify-center items-center border border-white my-8 p-6 rounded-md shadow-lg cursor-pointer hover:border-solid hover:border hover:border-[#34A853]"}>
       <div className="mt-2 mb-4">
-        <img src={ProductImage} alt={ProductName} className='w-full scale-100 object-contain transition-all duration-300 ease-in-out align-middle ' />
+        <img src={ProductImage} alt={ProductName} className='group-[.border-solid]:scale-125' />
       </div>
       <div className="w-full flex flex-col gap-4 justify-center items-center">
         <span className='font-bold text-2xl'>{ProductName}</span>
